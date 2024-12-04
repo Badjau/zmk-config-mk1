0abfcd1850fefd37201ac6a6bdd8e77095ae9744
Badjau <franzvasquez4153@gmail.com>
2024-12-05 01:39:20 +0800

    custom display initial test. kconfig.defconfig updated

--- config/boards/shields/nice_oled/widgets/animation.c
+++ config/boards/shields/nice_oled/widgets/animation.c
@@ -139,7 +139,7 @@ void draw_animation(lv_obj_t *canvas, struct zmk_widget_screen *widget) {
   lv_obj_center(art);
 
   lv_animimg_set_src(art, (const void **)anim_imgs, 25);
-  lv_animimg_set_duration(art, CONFIG_NICE_OLED_CUSTOM_MS);
+  lv_animimg_set_duration(art, CONFIG_NICE_OLED_CUSTOM_ANIMATION_MS);
   lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
   lv_animimg_start(art);
 
