342a96524a4cd614dc993a1cb57831c79caa2073
Badjau <franzvasquez4153@gmail.com>
2024-12-06 21:24:54 +0800

    profile count added

--- config/boards/shields/nice_oled/widgets/battery.c
+++ config/boards/shields/nice_oled/widgets/battery.c
@@ -41,7 +41,7 @@ static void draw_charging_level(lv_obj_t *canvas,
 static void draw_charging_status(lv_obj_t *canvas) {
   lv_draw_img_dsc_t img_dsc;
   lv_draw_img_dsc_init(&img_dsc);
-  lv_canvas_draw_img(canvas, 7, 32, &bolt, &img_dsc);
+  lv_canvas_draw_img(canvas, 7, 45, &bolt, &img_dsc);
 }
 
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
